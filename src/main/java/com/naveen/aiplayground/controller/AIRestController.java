c0505bc12811c44f285511ed3fcba8c8533f005a
iamnaveen93 <jagirdhar.naveen@icloud.com>
2026-04-09 01:19:44 +0530

    Update AIRestController.java

--- src/main/java/com/naveen/aiplayground/controller/AIRestController.java
+++ src/main/java/com/naveen/aiplayground/controller/AIRestController.java
@@ -15,7 +15,7 @@ public class AIRestController {
 
     private final Logger LOGGER = LoggerFactory.getLogger(AIRestController.class);
 
-    private final Map<String , AIModelClient> ABCDaiModelClientMap;
+    private final Map<String , AIModelClient> XYZaiModelClientMap;
 
     public AIRestController(Map<String, AIModelClient> aiModelClientMap) {
         this.aiModelClientMap = aiModelClientMap;
