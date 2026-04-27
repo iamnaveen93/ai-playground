a5bb0d69da41e2d5eb40be42cccd528105d1ba1b
iamnaveen93 <jagirdhar.naveen@icloud.com>
2026-04-27 22:38:51 +0530

    Update AIRestController.java

--- src/main/java/com/naveen/aiplayground/controller/AIRestController.java
+++ src/main/java/com/naveen/aiplayground/controller/AIRestController.java
@@ -5,7 +5,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import jakarta.validation.*;
+import jakarta.validation.Valid;
+import jakarta.validation.Notnull;
 
 import java.util.Map;
 import java.util.Optional;
